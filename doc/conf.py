fdbcaf7c7099b4c58a7aaec8ec3f1ae7bb383a44
Andreas Kloeckner <inform@tiker.net>
2020-12-02 18:57:45 -0600

    Black doc/conf

--- doc/conf.py
+++ doc/conf.py
@@ -30,29 +30,31 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+]
 autoclass_content = "class"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'logpyle'
-copyright = '2017, Andreas Kloeckner'
-author = 'Andreas Kloeckner'
+project = "logpyle"
+copyright = "2017, Andreas Kloeckner"
+author = "Andreas Kloeckner"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -60,8 +62,10 @@ author = 'Andreas Kloeckner'
 #
 # The short X.Y version.
 ver_dic = {}
-exec(compile(open("../logpyle/version.py").read(),
-    "../logpyle/version.py", 'exec'), ver_dic)
+exec(
+    compile(open("../logpyle/version.py").read(), "../logpyle/version.py", "exec"),
+    ver_dic,
+)
 version = ".".join(str(x) for x in ver_dic["VERSION"])
 release = ver_dic["VERSION_TEXT"]
 
@@ -75,10 +79,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -89,21 +93,21 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 html_theme_options = {
-        "extra_nav_links": {
-            "🚀 Github": "https://github.com/illinois-ceesd/logpyle",
-            "💾 Download Releases": "https://pypi.python.org/pypi/logpyle",
-            }
-        }
+    "extra_nav_links": {
+        "🚀 Github": "https://github.com/illinois-ceesd/logpyle",
+        "💾 Download Releases": "https://pypi.python.org/pypi/logpyle",
+    }
+}
 
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',
-        'searchbox.html',
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",
+        "searchbox.html",
     ]
 }
 
@@ -122,7 +126,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'logpyledoc'
+htmlhelp_basename = "logpyledoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -131,15 +135,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -149,8 +150,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'logpyle.tex', 'logpyle Documentation',
-     'Andreas Kloeckner', 'manual'),
+    (master_doc, "logpyle.tex", "logpyle Documentation", "Andreas Kloeckner", "manual"),
 ]
 
 
@@ -158,10 +158,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'logpyle', 'logpyle Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "logpyle", "logpyle Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -170,9 +167,15 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'logpyle', 'logpyle Documentation',
-     author, 'logpyle', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "logpyle",
+        "logpyle Documentation",
+        author,
+        "logpyle",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
 
@@ -180,4 +183,4 @@ intersphinx_mapping = {
     "https://docs.python.org/3": None,
     "https://numpy.org/doc/stable": None,
     "https://documen.tician.de/pymbolic/": None,
-    }
+}
