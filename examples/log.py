a6362a81b9266a336054ea20d1b00f9918b3b028
Matthias Diener <mdiener@illinois.edu>
2023-03-14 14:21:58 -0500

    isort (#83)

--- examples/log.py
+++ examples/log.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from time import sleep
 from random import uniform
-from logpyle import (LogManager, add_general_quantities,
-        add_simulation_quantities, add_run_info, IntervalTimer,
-        LogQuantity, set_dt, GCStats)
-
+from time import sleep
 from warnings import warn
+
+from logpyle import (GCStats, IntervalTimer, LogManager, LogQuantity,
+                     add_general_quantities, add_run_info,
+                     add_simulation_quantities, set_dt)
 
 
 class Fifteen(LogQuantity):
