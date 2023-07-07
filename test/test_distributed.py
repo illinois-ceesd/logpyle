710bb8a1d513a319269abeeb8e95bf5022d4810b
Mike Montano <edgesftw@gmail.com>
2023-07-07 18:42:50 -0500

    Primitive MPI test added

---
+++ test/test_distributed.py
@@ -0,0 +1,126 @@
+import pytest
+import sys
+import os
+import numpy as np
+
+from logpyle import (
+    add_run_info,
+    add_general_quantities,
+    add_simulation_quantities,
+    set_dt,
+    GCStats,
+    LogManager,
+    IntervalTimer,
+    PushLogQuantity,
+    TimestepDuration,
+    StepToStepDuration,
+    TimestepCounter,
+    WallTime,
+    LogQuantity,
+    CallableLogQuantityAdapter,
+    MultiLogQuantity,
+    DtConsumer,
+    ETA,
+    EventCounter,
+    time_and_count_function,
+)
+
+
+@pytest.fixture
+def basicLogmgr():
+    import os
+
+    # setup
+    filename = "THIS_LOG_SHOULD_BE_DELETED.sqlite"
+    logmgr = LogManager(filename, "wo")
+    # give obj to test
+    yield logmgr
+    # cleanup object
+    logmgr.close()
+    os.remove(filename)
+
+
+# {{{ mpi test infrastructure
+
+def run_test_with_mpi(num_ranks, f, *args, extra_env_vars=None):
+    pytest.importorskip("mpi4py")
+
+    if extra_env_vars is None:
+        extra_env_vars = {}
+
+    from pickle import dumps
+    from base64 import b64encode
+    from subprocess import check_call
+
+    env_vars = {
+            "RUN_WITHIN_MPI": "1",
+            "INVOCATION_INFO": b64encode(dumps((f, args))).decode(),
+            }
+    env_vars.update(extra_env_vars)
+
+    # NOTE: CI uses OpenMPI; -x to pass env vars. MPICH uses -env
+    check_call([
+        "mpiexec", "-np", str(num_ranks),
+        "--oversubscribe",
+        ] + [
+            item
+            for env_name, env_val in env_vars.items()
+            for item in ["-x", f"{env_name}={env_val}"]
+        ] + [sys.executable, "-m", "mpi4py", __file__])
+
+
+def run_test_with_mpi_inner():
+    from pickle import loads
+
+    from base64 import b64decode
+    f, args = loads(b64decode(os.environ["INVOCATION_INFO"].encode()))
+
+    f(*args)
+
+
+# }}}
+
+@pytest.mark.parametrize('execution_number', range(1))
+def test_distributed_execution_basic(execution_number):
+    run_test_with_mpi(2, _do_test_distributed_execution_basic)
+
+
+def _do_test_distributed_execution_basic():
+    from mpi4py import MPI  # pylint: disable=import-error
+
+    comm = MPI.COMM_WORLD
+
+    filename = "THIS_LOG_SHOULD_BE_DELETED.sqlite"
+    logmgr = LogManager(filename, "wu", comm)
+
+    # ------------------------------------------------------
+
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    print("Rank " + str(rank) + " of " + str(size))
+
+    print(str(rank), str(logmgr.rank))
+    assert rank == logmgr.rank
+    assert logmgr.is_parallel is True
+
+    # ------------------------------------------------------
+
+    logmgr.close()
+
+    def isUniqueFilename(str: str):
+        return str.startswith("THIS_LOG_SHOULD_BE_DELETED-")
+
+    files = [f for f in os.listdir() if isUniqueFilename(f)]
+    for f in files:
+        os.remove(f)
+
+
+if __name__ == "__main__":
+    if "RUN_WITHIN_MPI" in os.environ:
+        run_test_with_mpi_inner()
+    elif len(sys.argv) > 1:
+        exec(sys.argv[1])
+    else:
+        from pytest import main
+        main([__file__])
