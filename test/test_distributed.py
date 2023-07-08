d9fc8390bb47b482de8a5019462c782a678a875e
Mike Montano <edgesftw@gmail.com>
2023-07-07 19:04:46 -0500

    Distributed test for add_watches

--- test/test_distributed.py
+++ test/test_distributed.py
@@ -1,7 +1,6 @@
 import pytest
 import sys
 import os
-import numpy as np
 
 from logpyle import (
     add_run_info,
@@ -80,20 +79,34 @@ def run_test_with_mpi_inner():
 
 # }}}
 
-@pytest.mark.parametrize('execution_number', range(1))
-def test_distributed_execution_basic(execution_number):
-    run_test_with_mpi(2, _do_test_distributed_execution_basic)
-
-
-def _do_test_distributed_execution_basic():
+def setupManager() -> LogManager:
     from mpi4py import MPI  # pylint: disable=import-error
 
     comm = MPI.COMM_WORLD
 
     filename = "THIS_LOG_SHOULD_BE_DELETED.sqlite"
     logmgr = LogManager(filename, "wu", comm)
+    return logmgr, comm
 
-    # ------------------------------------------------------
+
+def teardownManager(logmgr: LogManager):
+    logmgr.close()
+
+    def isUniqueFilename(str: str):
+        return str.startswith("THIS_LOG_SHOULD_BE_DELETED-")
+
+    files = [f for f in os.listdir() if isUniqueFilename(f)]
+    for f in files:
+        os.remove(f)
+
+
+@pytest.mark.parametrize('execution_number', range(1))
+def test_distributed_execution_basic(execution_number):
+    run_test_with_mpi(2, _do_test_distributed_execution_basic)
+
+
+def _do_test_distributed_execution_basic():
+    logmgr, comm = setupManager()
 
     rank = comm.Get_rank()
     size = comm.Get_size()
@@ -104,16 +117,52 @@ def _do_test_distributed_execution_basic():
     assert rank == logmgr.rank
     assert logmgr.is_parallel is True
 
-    # ------------------------------------------------------
+    teardownManager(logmgr)
 
-    logmgr.close()
 
-    def isUniqueFilename(str: str):
-        return str.startswith("THIS_LOG_SHOULD_BE_DELETED-")
+@pytest.mark.parametrize('execution_number', range(1))
+def test_distributed_execution_add_watches(execution_number):
+    run_test_with_mpi(2, _do_test_distributed_execution_basic)
 
-    files = [f for f in os.listdir() if isUniqueFilename(f)]
-    for f in files:
-        os.remove(f)
+
+def _do_test_distributed_execution_add_watches():
+    logmgr, comm = setupManager()
+
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    print("Rank " + str(rank) + " of " + str(size))
+
+    class Fifteen(LogQuantity):
+        def __call__(self) -> int:
+            return 15
+
+    class FifteenStr(LogQuantity):
+        def __call__(self) -> str:
+            return "15.0"
+
+    logmgr.add_quantity(Fifteen("name1"))
+    logmgr.add_quantity(Fifteen("name2"))
+    logmgr.add_quantity(FifteenStr("tup_name1"))
+
+    watch_list = ["name1", ("tup_name1", "str"), "name2"]
+
+    logmgr.add_watches(watch_list)
+
+    logmgr.tick_before()
+    # do something ...
+    logmgr.tick_before()
+    logmgr.save()
+
+    # check that all watches are present
+    actualWatches = [watch.expr for watch in logmgr.watches]
+    expected = ["name1", "tup_name1", "name2"]
+    actualWatches.sort()
+    expected.sort()
+    print(actualWatches, expected)
+    assert actualWatches == expected
+
+    teardownManager(logmgr)
 
 
 if __name__ == "__main__":
