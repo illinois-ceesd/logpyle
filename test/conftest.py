5c988553a063fa2f1128344ada6a81a4068714a0
Mike Montano <edgesftw@gmail.com>
2023-08-04 11:30:06 -0500

    Moved assert_cov into conftest file

--- test/conftest.py
+++ test/conftest.py
@@ -15,3 +15,22 @@ def basic_logmgr():
     # cleanup object
     logmgr.close()
     os.remove(filename)
+
+def assert_cov(old_filename: str, new_filename: str):
+    import json
+
+    new_f = open(new_filename)
+    old_f = open(old_filename)
+
+    new = json.load(new_f)
+    old = json.load(old_f)
+
+    new_cov = new["totals"]["percent_covered"]
+    old_cov = old["totals"]["percent_covered"]
+
+    new_f.close()
+    old_f.close()
+    if new_cov < old_cov:
+        raise Exception(f"New coverage is less than old coverage. {old_cov}->{new_cov}")
+    else:
+        print("Test coverage is acceptable")
